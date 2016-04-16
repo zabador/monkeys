901d50124065e7a1fdd56f9cf1d845d68eadee95
schneis <skye.schneider@dominos.com>
2016-04-16 00:18:49 -0400

    Post every 5 minutes and start server code

--- client/main.go
+++ client/main.go
@@ -9,27 +9,47 @@ import (
 func main() {
 
 	flag := true
-	longest := make([]byte, 0, 8)
+	longestMatch := make([]byte, 0, 8)
+	var longestLength int
+
 
 	s := []byte{'m','o','n','k','e','y','s'}
 
 	count := 1
 
+
+	t0 := time.Now().UTC()
+	t1 := time.Now().UTC()
+
 	for flag {
 		for i := 0; i < len(s); i++ {
+
+			// get random int
 			s1 := rand.NewSource(time.Now().UnixNano())
 			r1 := rand.New(s1)
 			random := r1.Intn(123 - 97) + 97
+			// check if random int equals ASCII value of letter and post new longest match
 			if random == int(s[i]) {
-				if i > len(longest) {
-					longest = s[:i+1]
-					post(int64(count), longest)
+				if i > longestLength {
+					longestLength = i
+					longestMatch = s[:i+1]
+					post(int64(count), longestMatch)
 				}
+				// if we match all of monkeys then set flag to false
 				if i == len(s) - 1 {
 					flag = false
 				}
 			} else {
 				break
+			}
+			// check duration and post update every 5 minutes
+			d := time.Now().Sub(t1)
+			m := d.Minutes()
+			if m > 5.0 {
+				t1 = time.Now()
+				fmt.Println("Total duration:", time.Now().Sub(t0))
+				post(int64(count), longestMatch)
+
 			}
 		}
 		count++
